96f49d35d31165633c19607e100ffa17bd07cef9
kashish0903 <tomshreepali@gmail.com>
2026-01-23 14:01:36 +0545

    add the float concept

---
+++ mm.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int sum = 25, count = 5, mean1, x;
+    float mean2;
+    mean1 = sum / count;
+    printf(" value of mean1 :%d\n", mean1);
+    mean2 = mean1;
+    printf("value of mean2 : %f\n", mean2);
+    x = 7.5;
+    printf("x=%d", x);
+    getch();
+}
