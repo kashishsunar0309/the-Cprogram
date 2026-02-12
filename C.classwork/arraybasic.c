28bc3bb8433cd333a5a793461e94cae5dcc1afda
kashish0903 <tomshreepali@gmail.com>
2026-02-12 13:27:47 +0545

    add array basic

---
+++ C.classwork/arraybasic.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int myNumbers[] = {25, 50, 75, 100};
+    printf("%d\n", myNumbers[0]);
+    printf("%d\n", myNumbers[1]);
+    printf("%d\n", myNumbers[2]);
+    printf("%d\n", myNumbers[3]);
+    return 0;
+}
