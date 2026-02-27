2962cc7cb88304904e2cf98a960ae44d1b4bceba
kashish0903 <tomshreepali@gmail.com>
2026-02-27 20:56:27 +0545

    add Password strength checker

---
+++ password_checker.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+#include <ctype.h>
+int main()
+{
+    char password[100];
+    int hasUpper = 0, hasLower = 0, hasDigit = 0, hasSpecial = 0;
+    printf("===Password Strength Checker=== \n");
+    printf("Enter the Password: ");
+    scanf("%s", password);
+    int length = strlen(password);
+    for (int i = 0; i < length; i++)
+    {
+        if (isupper(password[i]))
+            hasUpper = 1;
+        else if (islower(password[i]))
+            hasLower = 1;
+        else if (isdigit(password[i]))
+            hasDigit = 1;
+        else
+            hasSpecial = 1;
+    }
+    printf("\n Password Strength: ");
+    if (length >= 8 && hasUpper && hasLower && hasDigit && hasSpecial)
+        printf("STRONG\n");
+    else if (length >= 6 && (hasUpper || hasLower) && hasDigit)
+        printf("MEDIUM\n");
+    else
+        printf("WEAK");
+    return 0;
+}
